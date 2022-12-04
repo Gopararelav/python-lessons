006abafb4f755aaa82b1edbcaf5b5ff6a484ccac
Gopararelav <valerakaza11@gmail.com>
2022-12-04 21:54:55 +0500

    Create homewwork_after_lesson1.py

---
+++ homewwork_after_lesson1.py
@@ -0,0 +1,54 @@
+def one():
+    print("I***like***Python")
+
+
+# one()
+
+
+def two():
+    print("1", "2", "3", sep="*")
+
+
+# two()
+
+
+def three():
+    print("31", "12", "2019", sep="-")
+
+
+# three()
+
+
+def four():
+    print("Mercury", "Venus!Mars", "*Jupiter?", sep="*")
+
+
+# four()
+
+
+def five():
+    print('a', 'b', 'c', sep='*') 
+    # печатается из-за end= "\n"
+    print('d', 'e', 'f', sep='**', end='')
+    print('g', 'h', 'i', sep='+', end='%')
+    print('j', 'k', 'l', sep='-', end='\n')
+    # печатается из за end= "\n"
+    print('m', 'n', 'o', sep='/', end='!')
+    print('p', 'q', 'r', sep='1', end='%')
+    print('s', 't', 'u', sep='&', end='\n')
+    # печатается из за end= "\n"
+    print('v', 'w', 'x', sep='%')
+    # печатается из за end= "\n"
+    print('y', 'z', sep='/', end='!')
+
+
+# five()
+
+
+def six():
+    print(" __________________\n|   rus  :   eng   |\n|------------------|")
+    print("|   один :   one   |")
+    print("|   два  :   two   |\n|   три  :   three |\n|__________________|\n")
+
+
+# six()
