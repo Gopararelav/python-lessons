32cf7d4afa32a0e7fc47e3d4095d517b6227ca2d
Валера <valerakaza11@gmail.com>
2023-03-03 18:57:30 +0500

    rhceg

---
+++ homework_after_lesson5.py
@@ -0,0 +1,36 @@
+def one():
+    num1 = []
+    while True:
+        c = int(input("введите число 0 = окончанию ввода: "))
+        if c != 0:
+            num1.append(c)
+        elif num1 == []:
+            print("невозможно найти среднее значение угля")
+            quit()
+        else:
+            break
+    h = 0
+    over = 0
+    for x in num1:
+        over += x
+        h += 1
+    print(over / h)
+
+
+# one()
+
+
+def two():
+    original = [4.95, 9.95, 14.95, 19.95, 24.95]
+    for x in original:
+        print("|$" + str(x), "$" + str(x-(x*0.6)), "$" + str(x*0.6), sep=",  ", end="|\n")
+
+
+# two()
+
+def CtoF():
+    for x in range(1, 101):
+        print("|C" + str(x), "=", "F" + str(((x*9)/5)+32), sep=" ", end="|\n")
+
+
+# CtoF()
